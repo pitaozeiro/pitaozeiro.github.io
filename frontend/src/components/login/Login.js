f15410f94b492785b75d20da3a2a434cc10ef488
Douglas Biscaia dos Santos <142869399+pitaozeiro@users.noreply.github.com>
2023-11-17 21:29:14 +0000

    teste

--- frontend/src/components/login/Login.js
+++ frontend/src/components/login/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
       <header className="section-information">
         <h1 className="b-title">Bem Vindo!</h1>
         <p className="info-text">Este aplicativo é apenas um portfólio que criei com o intuito em treinar e entrar na área de desenvolvimento de softwares, portanto não insira informações reais, nunca solicitaremos seus dados, pagamentos ou quaisquer outras ações.</p>
-        <p className="info-text-login">Usuário: admin Senha: @admin123</p>
+        <p className="info-text-login">Usuário: admin Senha: ********</p>
       </header>
       <section>
         <div className="container-login-form">
